ffb1528e62cb5e630623e03ef19453f21cbf0885
Michael Neumann <mneumann@ntecs.de>
2015-02-14 11:25:39 +0100

    drm: Bring in HDMI support from Linux

--- sys/dev/drm/radeon/r600_hdmi.c
+++ sys/dev/drm/radeon/r600_hdmi.c
@@ -26,6 +26,7 @@
  * $FreeBSD: head/sys/dev/drm2/radeon/r600_hdmi.c 254885 2013-08-25 19:37:15Z dumbbell $
  */
 
+#include <linux/hdmi.h>
 #include <drm/drmP.h>
 #include <uapi_drm/radeon_drm.h>
 #include "radeon.h"
@@ -134,7 +135,7 @@ static void r600_hdmi_update_avi_infoframe(struct drm_encoder *encoder,
 	struct radeon_encoder *radeon_encoder = to_radeon_encoder(encoder);
 	struct radeon_encoder_atom_dig *dig = radeon_encoder->enc_priv;
 	uint32_t offset = dig->afmt->offset;
-	uint8_t *frame = buffer + 3;
+	uint8_t *frame = (uint8_t*)buffer + 3;
 
 	/* Our header values (type, version, length) should be alright, Intel
 	 * is using the same. Checksum function also seems to be OK, it works
@@ -165,7 +166,7 @@ static void r600_hdmi_update_audio_infoframe(struct drm_encoder *encoder,
 	struct radeon_encoder *radeon_encoder = to_radeon_encoder(encoder);
 	struct radeon_encoder_atom_dig *dig = radeon_encoder->enc_priv;
 	uint32_t offset = dig->afmt->offset;
-	const u8 *frame = buffer + 3;
+	const u8 *frame = (const u8*)buffer + 3;
 
 	WREG32(HDMI0_AUDIO_INFO0 + offset,
 		frame[0x0] | (frame[0x1] << 8) | (frame[0x2] << 16) | (frame[0x3] << 24));
