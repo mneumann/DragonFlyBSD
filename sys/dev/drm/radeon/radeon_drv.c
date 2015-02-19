09d25abc785fa2f2567715caa0d12974a62853f3
Michael Neumann <mneumann@ntecs.de>
2015-02-19 12:58:52 +0100

    drm/radeon: add ring working query
    Linux-commit: 902aaef6c698ce0f04a6acab2d5396519d5de330

--- sys/dev/drm/radeon/radeon_drv.c
+++ sys/dev/drm/radeon/radeon_drv.c
@@ -74,9 +74,10 @@
  *   2.29.0 - R500 FP16 color clear registers
  *   2.30.0 - fix for FMASK texturing
  *   2.31.0 - Add fastfb support for rs690
+ *   2.32.0 - new info request for rings working
  */
 #define KMS_DRIVER_MAJOR	2
-#define KMS_DRIVER_MINOR	31
+#define KMS_DRIVER_MINOR	32
 #define KMS_DRIVER_PATCHLEVEL	0
 int radeon_suspend_kms(struct drm_device *dev);
 int radeon_resume_kms(struct drm_device *dev);
