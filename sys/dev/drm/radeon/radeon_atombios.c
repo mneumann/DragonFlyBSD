f53968658bd805c3a074ff65f189cecc95bf58b6
Michael Neumann <mneumann@ntecs.de>
2015-02-20 11:27:34 +0100

    drm/radeon: fix possible segfault when parsing pm tables
    Linux-commit: f8e6bfc2ce162855fa4f9822a45659f4b542c960

--- sys/dev/drm/radeon/radeon_atombios.c
+++ sys/dev/drm/radeon/radeon_atombios.c
@@ -2012,6 +2012,8 @@ static int radeon_atombios_parse_power_table_1_3(struct radeon_device *rdev)
 	num_modes = power_info->info.ucNumOfPowerModeEntries;
 	if (num_modes > ATOM_MAX_NUMBEROF_POWER_BLOCK)
 		num_modes = ATOM_MAX_NUMBEROF_POWER_BLOCK;
+	if (num_modes == 0)
+		return state_index;
 	rdev->pm.power_state = kmalloc(sizeof(struct radeon_power_state) * num_modes,
 	    M_DRM, M_WAITOK | M_ZERO);
 	if (!rdev->pm.power_state)
@@ -2420,6 +2422,8 @@ static int radeon_atombios_parse_power_table_4_5(struct radeon_device *rdev)
 	power_info = (union power_info *)((char *)mode_info->atom_context->bios + data_offset);
 
 	radeon_atombios_add_pplib_thermal_controller(rdev, &power_info->pplib.sThermalController);
+	if (power_info->pplib.ucNumStates == 0)
+		return state_index;
 	rdev->pm.power_state = kmalloc(sizeof(struct radeon_power_state) *
 				       power_info->pplib.ucNumStates,
 				       M_DRM, M_WAITOK | M_ZERO);
@@ -2519,6 +2523,8 @@ static int radeon_atombios_parse_power_table_6(struct radeon_device *rdev)
 	non_clock_info_array = (struct _NonClockInfoArray *)
 		((char *)mode_info->atom_context->bios + data_offset +
 		 le16_to_cpu(power_info->pplib.usNonClockInfoArrayOffset));
+	if (state_array->ucNumEntries == 0)
+		return state_index;
 	rdev->pm.power_state = kmalloc(sizeof(struct radeon_power_state) *
 				       state_array->ucNumEntries,
 				       M_DRM, M_WAITOK | M_ZERO);
@@ -2610,7 +2616,9 @@ void radeon_atombios_get_power_modes(struct radeon_device *rdev)
 		default:
 			break;
 		}
-	} else {
+	}
+
+	if (state_index == 0) {
 		rdev->pm.power_state = kmalloc(sizeof(struct radeon_power_state),
 		    M_DRM, M_WAITOK | M_ZERO);
 		if (rdev->pm.power_state) {
