0a772bb670fa8122ac5a46d7fb0a26d2966b3f9e
Michael Neumann <mneumann@ntecs.de>
2015-02-27 15:38:24 +0000

    drm/radeon: [082/274] kms: add dpm support for trinity asics [3.11]
    Linux-commit: d70229f704474b2932e03367a528773e336f6205 [url]
    
    [url]: http://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/commit/?id=d70229f704474b2932e03367a528773e336f6205

--- sys/dev/drm/radeon/radeon_asic.h
+++ sys/dev/drm/radeon/radeon_asic.h
@@ -596,6 +596,18 @@ void cayman_dma_vm_flush(struct radeon_device *rdev, int ridx, struct radeon_vm 
 extern void cayman_cp_int_cntl_setup(struct radeon_device *rdev,
 				     int ring, u32 cp_int_cntl);
 
+int trinity_dpm_init(struct radeon_device *rdev);
+int trinity_dpm_enable(struct radeon_device *rdev);
+void trinity_dpm_disable(struct radeon_device *rdev);
+int trinity_dpm_set_power_state(struct radeon_device *rdev);
+void trinity_dpm_setup_asic(struct radeon_device *rdev);
+void trinity_dpm_display_configuration_changed(struct radeon_device *rdev);
+void trinity_dpm_fini(struct radeon_device *rdev);
+u32 trinity_dpm_get_sclk(struct radeon_device *rdev, bool low);
+u32 trinity_dpm_get_mclk(struct radeon_device *rdev, bool low);
+void trinity_dpm_print_power_state(struct radeon_device *rdev,
+				   struct radeon_ps *ps);
+
 /* DCE6 - SI */
 void dce6_bandwidth_update(struct radeon_device *rdev);
 
