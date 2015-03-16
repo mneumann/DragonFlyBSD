aac853c8515e613549c62b285656e83f73eb1b93
Michael Neumann <mneumann@ntecs.de>
2015-03-16 13:28:46 +0000

    drm/radeon: [204/274] dpm: add checks against vblank time [3.11]
    Linux-commit: 48783069350a2963e97696a3c3ed0a40cbe35210 [url]
    
    [url]: http://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/commit/?id=48783069350a2963e97696a3c3ed0a40cbe35210

--- sys/dev/drm/radeon/radeon_pm.c
+++ sys/dev/drm/radeon/radeon_pm.c
@@ -646,6 +646,14 @@ static struct radeon_ps *radeon_dpm_pick_power_state(struct radeon_device *rdev,
 	int i;
 	struct radeon_ps *ps;
 	u32 ui_class;
+	bool single_display = (rdev->pm.dpm.new_active_crtc_count < 2) ?
+		true : false;
+
+	/* check if the vblank period is too short to adjust the mclk */
+	if (single_display && rdev->asic->dpm.vblank_too_short) {
+		if (radeon_dpm_vblank_too_short(rdev))
+			single_display = false;
+	}
 
 	/* certain older asics have a separare 3D performance state,
 	 * so try that first if the user selected performance
@@ -666,7 +674,7 @@ restart_search:
 		case POWER_STATE_TYPE_BATTERY:
 			if (ui_class == ATOM_PPLIB_CLASSIFICATION_UI_BATTERY) {
 				if (ps->caps & ATOM_PPLIB_SINGLE_DISPLAY_ONLY) {
-					if (rdev->pm.dpm.new_active_crtc_count < 2)
+					if (single_display)
 						return ps;
 				} else
 					return ps;
@@ -675,7 +683,7 @@ restart_search:
 		case POWER_STATE_TYPE_BALANCED:
 			if (ui_class == ATOM_PPLIB_CLASSIFICATION_UI_BALANCED) {
 				if (ps->caps & ATOM_PPLIB_SINGLE_DISPLAY_ONLY) {
-					if (rdev->pm.dpm.new_active_crtc_count < 2)
+					if (single_display)
 						return ps;
 				} else
 					return ps;
@@ -684,7 +692,7 @@ restart_search:
 		case POWER_STATE_TYPE_PERFORMANCE:
 			if (ui_class == ATOM_PPLIB_CLASSIFICATION_UI_PERFORMANCE) {
 				if (ps->caps & ATOM_PPLIB_SINGLE_DISPLAY_ONLY) {
-					if (rdev->pm.dpm.new_active_crtc_count < 2)
+					if (single_display)
 						return ps;
 				} else
 					return ps;
