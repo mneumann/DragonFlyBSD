e8ff2e19e3d3d55c1786d3c702b82ec903e24baa
Michael Neumann <mneumann@ntecs.de>
2015-03-16 13:29:36 +0000

    drm/radeon: [205/274] dpm: implement vblank_too_short callback for 7xx [3.11]
    Linux-commit: b06195d9943d561273939e866b4bfc87beba18bc [url]
    
    [url]: http://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/commit/?id=b06195d9943d561273939e866b4bfc87beba18bc

--- sys/dev/drm/radeon/radeon_asic.h
+++ sys/dev/drm/radeon/radeon_asic.h
@@ -474,6 +474,7 @@ void rv770_dpm_print_power_state(struct radeon_device *rdev,
 				 struct radeon_ps *ps);
 void rv770_dpm_debugfs_print_current_performance_level(struct radeon_device *rdev,
 						       struct seq_file *m);
+bool rv770_dpm_vblank_too_short(struct radeon_device *rdev);
 
 /*
  * evergreen
