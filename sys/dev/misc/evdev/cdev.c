12ac6b7825fba8f53f9cbfe3a6aaed27983c1848
Michael Neumann <mneumann@ntecs.de>
2024-04-09 16:08:28 +0200

    evdev - Linux evdev does not terminate truncated strings with 0
    It seems that DragonFly ioctl(2) cannot return the actual length of
    copyouted data as only error=0 is treated as "no-error".
    
    Based-on-FreeBSD-commit: https://github.com/freebsd/freebsd-src/commit/7a810290b8f6c6885fdb9917cf590d46fa270a61

--- sys/dev/misc/evdev/cdev.c
+++ sys/dev/misc/evdev/cdev.c
@@ -513,21 +513,25 @@ evdev_ioctl(struct dev_ioctl_args *ap)
 	/* evdev variable-length ioctls handling */
 	switch (IOCBASECMD(cmd)) {
 	case EVIOCGNAME(0):
-		strlcpy(data, evdev->ev_name, len);
+		/* Linux evdev does not terminate truncated strings with 0 */
+		limit = MIN(strlen(evdev->ev_name) + 1, len);
+		memcpy(data, evdev->ev_name, limit);
 		return (0);
 
 	case EVIOCGPHYS(0):
 		if (evdev->ev_shortname[0] == 0)
 			return (ENOENT);
 
-		strlcpy(data, evdev->ev_shortname, len);
+		limit = MIN(strlen(evdev->ev_shortname) + 1, len);
+		memcpy(data, evdev->ev_shortname, limit);
 		return (0);
 
 	case EVIOCGUNIQ(0):
 		if (evdev->ev_serial[0] == 0)
 			return (ENOENT);
 
-		strlcpy(data, evdev->ev_serial, len);
+		limit = MIN(strlen(evdev->ev_serial) + 1, len);
+		memcpy(data, evdev->ev_serial, limit);
 		return (0);
 
 	case EVIOCGPROP(0):
@@ -536,6 +540,7 @@ evdev_ioctl(struct dev_ioctl_args *ap)
 		return (0);
 
 	case EVIOCGMTSLOTS(0):
+		/* EVIOCGMTSLOTS always returns 0 on success */
 		if (evdev->ev_mt == NULL)
 			return (EINVAL);
 		if (len < sizeof(uint32_t))
