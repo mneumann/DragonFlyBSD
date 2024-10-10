a5f0974124a1384d4d7654d2439c6856006b12f7
Michael Neumann <mneumann@ntecs.de>
2024-10-10 10:55:04 +0200

    Retire crypto(9) and tcplay(8) and refactor dm_target_crypt
    * Completely remove the OpenBSD-inherited in-kernel crypto(9) framework
      that lived in sys/opencrypto and hardware drivers in dev/crypto. With it,
      remove /dev/crypto - the only application relying on it in DragonFly
      was tcplay...
    
    * Remove tcplay(8). It was severly broken and could panic the kernel by
      exceeding the malloc limits; see https://bugs.dragonflybsd.org/issues/3370
    
    * Remove hifn(4), safe(4), ubsec(4) and padlock(4) drivers. These  were
      removed in OpenBSD 7.1 and it's unlikely that they are still in use by
      someone.
    
    * In-place of crypto(9), add a simple non-allocating API that lives in
      crypto_ciphers.{h,c}. It's only used by dm_target_crypt.
    
    * Remove support for the following crypto algorithms:
    
      Blowfish, Camellia, Cast, Serpent, Skipjack, Twofish
    
      Mainly, because I am too lazy to test any of these with
      dm_target_crypt, but also, because there is no hardware support for
      any of these.
    
    * Greatly refactor dm_target_crypt. It used to encrypt/decrypt blocks in
      pieces of 512 bytes and send them over to the async crypto(9) framework.
      The async-ness of crypto(9) led to overly complex code. The new
      code processes each BIO read or write request by a worker thread.
      There are ncpu kthreads to process read requests, and another
      set of ncpu kthreads to process write requests. Instead of using a
      malloc_pipe as before, and allocating memory for each BIO request
      from that mpipe, each worker kthread now simply owns the required
      memory. That being said, it is 100% non-allocating.
      The BIO requests are distributing to the worker threads round robin.
      Performance-wise it is about the same speed as before, slightly better,
      whereas interactivity is a lot better now - mainly concerning the mouse
      cursor, which keeps on moving rather smooth, even under higher load.
      Under very high I/O, the rest of the system still because very slow,
      but the same happens without dm_target_crypt.
      Note that I test using a rather old i5-8350U Panasoci laptop that
      still has SATA SSD.
    
    * The AESNI (hardware instruction for AES as found in most Intel/AMD CPUs)
      code moved from dev/aesni to crypto/. It is no longer a device.
      We simply check for the CPU feature when "probing" for available
      crypto ciphers. Note that by default, the hardware-accelerated
      ciphers (AES-CBC, AES-XTS) are prefered over the software
      implementation. You can disable aesni by setting sysctl
      hw.aesni_disable=1. Note that, setting this will not have immediate
      effect for already "mounted" dm_target_crypt targets, as the crypto
      cipher is queried only at attach time.
    
    * Use explicit_bzero in a few places.
    
    * I've tested both AES-CBC and AES-XTS with and w/o AESNI. Basically, I
      formatted a device (cryptsetup luksFormat) with the old kernel, write
      some data on it, then switched to the new kernel, and tried to open it.

---
+++ sys/crypto/crypto_cipher.c
@@ -0,0 +1,389 @@
+/*-
+ * The authors of this code are John Ioannidis (ji@tla.org),
+ * Angelos D. Keromytis (kermit@csd.uch.gr) and
+ * Niels Provos (provos@physnet.uni-hamburg.de).
+ *
+ * This code was written by John Ioannidis for BSD/OS in Athens, Greece,
+ * in November 1995.
+ *
+ * Ported to OpenBSD and NetBSD, with additional transforms, in December
+ * 1996, by Angelos D. Keromytis.
+ *
+ * Additional transforms and features in 1997 and 1998 by Angelos D.
+ * Keromytis and Niels Provos.
+ *
+ * Additional features in 1999 by Angelos D. Keromytis.
+ *
+ * Copyright (C) 1995, 1996, 1997, 1998, 1999 by John Ioannidis,
+ * Angelos D. Keromytis and Niels Provos.
+ *
+ * Copyright (C) 2001, Angelos D. Keromytis.
+ *
+ * Copyright (c) 2024, Michael Neumann.
+ *
+ * Permission to use, copy, and modify this software with or without fee
+ * is hereby granted, provided that this entire notice is included in
+ * all copies of any software which is or includes a copy or
+ * modification of this software.
+ * You may use this code under the GNU public license if you so wish.
+ * Please contribute changes back to the authors under this freer than
+ * GPL license so that we may further the use of strong encryption
+ * without limitations to all.
+ *
+ * THIS SOFTWARE IS BEING PROVIDED "AS IS", WITHOUT ANY EXPRESS OR
+ * IMPLIED WARRANTY. IN PARTICULAR, NONE OF THE AUTHORS MAKES ANY
+ * REPRESENTATION OR WARRANTY OF ANY KIND CONCERNING THE
+ * MERCHANTABILITY OF THIS SOFTWARE OR ITS FITNESS FOR ANY PARTICULAR
+ * PURPOSE.
+ */
+
+#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/systm.h>
+
+#include <crypto/crypto_cipher.h>
+
+/**
+ * --------------------------------------
+ *  Utility
+ * --------------------------------------
+ */
+
+static inline void
+xor_block(uint8_t *dst, const uint8_t *src, int blocksize)
+{
+	for (int i = 0; i < blocksize; i++)
+		dst[i] ^= src[i];
+}
+
+typedef void (*block_fn_t)(const void *ctx, uint8_t *data, uint8_t *data2);
+
+/*
+ * XOR with the IV/previous block, as appropriate.
+ */
+#define ENCRYPT_DATA_CBC(block_fn, ctx, data, datalen, blocksize, iv)       \
+	for (int i = 0; i < datalen; i += blocksize) {                      \
+		xor_block(data + i, (i == 0) ? iv : (data + i - blocksize), \
+		    blocksize);                                             \
+		block_fn(ctx, data + i, data + i);                          \
+	}
+
+/*
+ * Start at the end, so we don't need to keep the
+ * encrypted block as the IV for the next block.
+ *
+ * XOR with the IV/previous block, as appropriate
+ */
+#define DECRYPT_DATA_CBC(block_fn, ctx, data, datalen, blocksize, iv)       \
+	for (int i = datalen - blocksize; i >= 0; i -= blocksize) {         \
+		block_fn(ctx, data + i, data + i);                          \
+		xor_block(data + i, (i == 0) ? iv : (data + i - blocksize), \
+		    blocksize);                                             \
+	}
+
+/**
+ * --------------------------------------
+ * Cipher null
+ * --------------------------------------
+ */
+
+static int
+cipher_null_probe(const char *algo_name, const char *mode_name __unused,
+    int keysize_in_bits __unused)
+{
+	if (strcmp(algo_name, "null") == 0)
+		return (0);
+	return (-1);
+}
+
+static int
+cipher_null_setkey(struct crypto_cipher_context *ctx __unused,
+    const uint8_t *keydata __unused, int keylen_in_bytes __unused)
+{
+	return (0);
+}
+
+static int
+cipher_null_encrypt(const struct crypto_cipher_context *ctx __unused,
+    uint8_t *data __unused, int datalen __unused,
+    struct crypto_cipher_iv *iv __unused)
+{
+	return (0);
+}
+
+static int
+cipher_null_decrypt(const struct crypto_cipher_context *ctx __unused,
+    uint8_t *data, int datalen, struct crypto_cipher_iv *iv __unused)
+{
+	return (0);
+}
+
+const struct crypto_cipher cipher_null = {
+	"null",
+	"null",
+	4,
+	0,
+	0,
+	cipher_null_probe,
+	cipher_null_setkey,
+	cipher_null_encrypt,
+	cipher_null_decrypt,
+};
+
+/**
+ * --------------------------------------
+ * AES-CBC (Rijndael-128)
+ * --------------------------------------
+ */
+
+#define AES_BLOCK_LEN 16
+
+static int
+aes_cbc_probe(const char *algo_name, const char *mode_name, int keysize_in_bits)
+{
+	if ((strcmp(algo_name, "aes") == 0) &&
+	    (strcmp(mode_name, "cbc") == 0) &&
+	    (keysize_in_bits == 128 || keysize_in_bits == 192 ||
+		keysize_in_bits == 256))
+		return (0);
+	else
+		return (-1);
+}
+
+static int
+aes_cbc_setkey(struct crypto_cipher_context *ctx, const uint8_t *keydata,
+    int keylen_in_bytes)
+{
+	switch (keylen_in_bytes * 8) {
+	case 128:
+	case 192:
+	case 256:
+		rijndael_set_key((void *)ctx, keydata, keylen_in_bytes * 8);
+		return (0);
+
+	default:
+		return (EINVAL);
+	}
+}
+
+static int
+aes_cbc_encrypt(const struct crypto_cipher_context *ctx, uint8_t *data,
+    int datalen, struct crypto_cipher_iv *iv)
+{
+	if ((datalen % AES_BLOCK_LEN) != 0)
+		return EINVAL;
+
+	ENCRYPT_DATA_CBC(rijndael_encrypt, (const void *)ctx, data, datalen,
+	    AES_BLOCK_LEN, (uint8_t *)iv);
+
+	return (0);
+}
+
+static int
+aes_cbc_decrypt(const struct crypto_cipher_context *ctx, uint8_t *data,
+    int datalen, struct crypto_cipher_iv *iv)
+{
+	if ((datalen % AES_BLOCK_LEN) != 0)
+		return EINVAL;
+
+	DECRYPT_DATA_CBC(rijndael_decrypt, (const void *)ctx, data, datalen,
+	    AES_BLOCK_LEN, (uint8_t *)iv);
+
+	return (0);
+}
+
+const struct crypto_cipher cipher_aes_cbc = {
+	"aes-cbc",
+	"AES-CBC (Rijndael-128) in software",
+	AES_BLOCK_LEN,
+	AES_BLOCK_LEN,
+	sizeof(rijndael_ctx),
+	aes_cbc_probe,
+	aes_cbc_setkey,
+	aes_cbc_encrypt,
+	aes_cbc_decrypt,
+};
+
+/**
+ * --------------------------------------
+ * AES-XTS
+ * --------------------------------------
+ */
+
+#define AES_XTS_BLOCK_LEN 16
+#define AES_XTS_IV_LEN	  8
+#define AES_XTS_ALPHA	  0x87 /* GF(2^128) generator polynomial */
+
+static void
+aes_xts_crypt_block(const struct aes_xts_ctx *ctx, uint8_t *data, uint8_t *iv,
+    bool do_encrypt)
+{
+	uint8_t block[AES_XTS_BLOCK_LEN];
+	u_int i, carry_in, carry_out;
+
+	for (i = 0; i < AES_XTS_BLOCK_LEN; i++)
+		block[i] = data[i] ^ iv[i];
+
+	if (do_encrypt)
+		rijndael_encrypt(&ctx->key1, block, data);
+	else
+		rijndael_decrypt(&ctx->key1, block, data);
+
+	for (i = 0; i < AES_XTS_BLOCK_LEN; i++)
+		data[i] ^= iv[i];
+
+	/* Exponentiate tweak */
+	carry_in = 0;
+	for (i = 0; i < AES_XTS_BLOCK_LEN; i++) {
+		carry_out = iv[i] & 0x80;
+		iv[i] = (iv[i] << 1) | (carry_in ? 1 : 0);
+		carry_in = carry_out;
+	}
+	if (carry_in)
+		iv[0] ^= AES_XTS_ALPHA;
+	explicit_bzero(block, sizeof(block));
+}
+
+static bool
+aes_xts_valid_keysize_in_bits(int keysize_in_bits)
+{
+	switch (keysize_in_bits) {
+	case 256:
+	case 512:
+		return true;
+	default:
+		return false;
+	}
+}
+
+static int
+aes_xts_probe(const char *algo_name, const char *mode_name, int keysize_in_bits)
+{
+	if (strcmp(algo_name, "aes") != 0)
+		return (-1);
+
+	if (strcmp(mode_name, "xts") != 0)
+		return (-1);
+
+	if (aes_xts_valid_keysize_in_bits(keysize_in_bits))
+		return (0);
+
+	return (-1);
+}
+
+static int
+aes_xts_setkey(struct crypto_cipher_context *ctx, const uint8_t *keydata,
+    int keylen_in_bytes)
+{
+	if (!aes_xts_valid_keysize_in_bits(keylen_in_bytes * 8))
+		return (EINVAL);
+
+	rijndael_set_key(&ctx->_ctx._aes_xts.key1, keydata,
+	    (keylen_in_bytes / 2) * 8);
+	rijndael_set_key(&ctx->_ctx._aes_xts.key2,
+	    keydata + (keylen_in_bytes / 2), (keylen_in_bytes / 2) * 8);
+
+	return (0);
+}
+
+static void
+aes_xts_reinit(const struct aes_xts_ctx *ctx, u_int8_t *iv)
+{
+#if 0
+	/*
+	 * Prepare tweak as E_k2(IV). IV is specified as LE representation
+	 * of a 64-bit block number which we allow to be passed in directly.
+	 */
+	/* XXX: possibly use htole64? */
+#endif
+	/* Last 64 bits of IV are always zero */
+	bzero(iv + AES_XTS_IV_LEN, AES_XTS_IV_LEN);
+
+	rijndael_encrypt(&ctx->key2, iv, iv);
+}
+
+static int
+aes_xts_encrypt(const struct crypto_cipher_context *_ctx, uint8_t *data,
+    int datalen, struct crypto_cipher_iv *_iv)
+{
+	uint8_t *iv = _iv->_iv._aes_xts;
+	const struct aes_xts_ctx *ctx = &_ctx->_ctx._aes_xts;
+
+	if ((datalen % AES_XTS_BLOCK_LEN) != 0)
+		return EINVAL;
+
+	aes_xts_reinit(ctx, iv);
+	for (int i = 0; i < datalen; i += AES_XTS_BLOCK_LEN) {
+		aes_xts_crypt_block(ctx, data + i, iv, true);
+	}
+
+	return (0);
+}
+
+static int
+aes_xts_decrypt(const struct crypto_cipher_context *_ctx, uint8_t *data,
+    int datalen, struct crypto_cipher_iv *_iv)
+{
+	uint8_t *iv = _iv->_iv._aes_xts;
+	const struct aes_xts_ctx *ctx = &_ctx->_ctx._aes_xts;
+
+	if ((datalen % AES_XTS_BLOCK_LEN) != 0)
+		return EINVAL;
+
+	aes_xts_reinit(ctx, iv);
+	for (int i = 0; i < datalen; i += AES_XTS_BLOCK_LEN) {
+		aes_xts_crypt_block(ctx, data + i, iv, false);
+	}
+
+	return (0);
+}
+
+const struct crypto_cipher cipher_aes_xts = {
+	"aes-xts",
+	"AES-XTS (in software)",
+	AES_XTS_BLOCK_LEN,
+	16,
+	sizeof(struct aes_xts_ctx),
+	aes_xts_probe,
+	aes_xts_setkey,
+	aes_xts_encrypt,
+	aes_xts_decrypt,
+};
+
+/**
+ *
+ */
+
+const struct crypto_cipher *crypto_ciphers[6] = {
+
+	&cipher_null,
+
+	/* first probe AESNI, then fallback to software AES */
+	&cipher_aesni_cbc, &cipher_aesni_xts,
+
+	/* AES in software */
+	&cipher_aes_cbc, &cipher_aes_xts,
+
+	NULL
+};
+
+/**
+ * --------------------------------------
+ * --------------------------------------
+ */
+
+const struct crypto_cipher *
+crypto_cipher_find(const char *algo_name, const char *mode_name,
+    int keysize_in_bits)
+{
+	for (const struct crypto_cipher **cipherpp = crypto_ciphers; *cipherpp;
+	     ++cipherpp) {
+		const struct crypto_cipher *cipherp = *cipherpp;
+		if ((*cipherp->probe)(algo_name, mode_name, keysize_in_bits) ==
+		    0) {
+			return cipherp;
+		}
+	}
+
+	return NULL;
+}
