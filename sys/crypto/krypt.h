7b7970ce52b4333516efe234131b8d20d940ae99
Michael Neumann <mneumann@ntecs.de>
2024-10-17 11:52:13 +0200

    refactor

--- sys/crypto/krypt.h
+++ sys/crypto/krypt.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-struct krypt_cipher_def {
+struct krypt_cipher {
 	const char *name;
 	uint16_t blocksize;
 	uint16_t ctxsize;
@@ -18,17 +18,17 @@ struct krypt_cipher_def {
 
 struct krypt_ctx {
 	int krypt_flags;
-	const struct krypt_cipher_def *krypt_cipher;
+	const struct krypt_cipher *krypt_cipher;
 	void *krypt_ctx;
 	uint8_t *krypt_iv;
 };
 
 typedef struct krypt_ctx *krypt_ctx_t;
 
-const struct krypt_cipher_def *krypt_find_cipher(const char *cipher_name,
+const struct krypt_cipher *krypt_find_cipher(const char *cipher_name,
     int keysize_in_bits);
 
-int krypt_init(krypt_ctx_t ctx, const struct krypt_cipher_def *cipher);
+int krypt_init(krypt_ctx_t ctx, const struct krypt_cipher *cipher);
 int krypt_setkey(krypt_ctx_t ctx, const uint8_t *keydata, int keylen);
 int krypt_setiv(krypt_ctx_t ctx, const uint8_t *ivdata, int ivlen);
 int krypt_encrypt(krypt_ctx_t ctx, uint8_t *data, int datalen);
