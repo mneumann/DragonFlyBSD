653bb3df292bac05349aa250706443f7f5463f53
Michael Neumann <mneumann@ntecs.de>
2025-03-30 20:51:14 +0200

    bzero write mpipe buffers on unload

--- sys/dev/disk/dm/crypt/dm_target_crypt.c
+++ sys/dev/disk/dm/crypt/dm_target_crypt.c
@@ -75,7 +75,7 @@ SYSCTL_INT(_dev_dm_crypt, OID_AUTO, readqueue_depth,
 /**
  * A work pool implementation.
  *
- * Use one queue per worker.
+ * Uses one queue per worker.
  */
 
 #define WORKER_JOB_FLAG_FREE		0x01
@@ -605,6 +605,13 @@ dmtc_get_nmax(void)
 }
 
 static void
+dmtc_deconstruct_write_mpipe_buf(void *buf, void *priv)
+{
+	(void)priv;
+	explicit_bzero(buf, DMTC_BUF_SIZE);
+}
+
+static void
 dmtc_init_mpipe(struct target_crypt_config *priv)
 {
 	int nmax = dmtc_get_nmax();
@@ -612,8 +619,8 @@ dmtc_init_mpipe(struct target_crypt_config *priv)
 
 	kprintf("dm_target_crypt: Setting %d mpipe write buffers\n", writebuf_count);
 	mpipe_init(&priv->write_mpipe, M_DMCRYPT, DMTC_BUF_SIZE,
-		writebuf_count, writebuf_count, MPF_NOZERO | MPF_CALLBACK,
-		NULL, NULL, NULL);
+		writebuf_count, writebuf_count, MPF_NOZERO | MPF_CACHEDATA,
+		NULL, dmtc_deconstruct_write_mpipe_buf, NULL);
 }
 
 static void
